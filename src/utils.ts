63992f62c651fcf6a7d06505356358d1c0f96aea
LayintonDev <layintondev@gmail.com>
2024-10-05 10:58:07 +0100

    initial commit

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,11 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import { User } from "../generated/schema";
 
 export function loadOrCreateUser(id: string): User {
     let user = User.load(id);
     if (user === null) {
         user = new User(id);
+        user.balance = BigInt.fromI32(0);
         user.save();
     }
 
